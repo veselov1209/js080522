e0a460dd68d341d29af3d16541046c01d2de9f68
veselov1209 <veselov1209@gmail.com>
2022-05-08 10:17:42 +0300

    console log

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+//alert("Hello, World!");
+
+/*
+console.log("Hello, World");
+console.log("Hello", "Java Script", "!!!!!!")
+*/
+
+//переменные
+let x;
+let y;
+let userName;
+let userColorAvatarBG;
+let a, b, c;
+
+x = 5;
+y = -100;
+a = 3.14;
+b = 0.00005;
+userName = "Vasya";
+userColorAvatarBG = "red";
+
+console.log(x);
+console.log(y, a, b);
+console.log("User name is", userName);
+console.log("a =", a);
