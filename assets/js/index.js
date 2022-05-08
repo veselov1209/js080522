af2543e2e07518e3f932f186f0162879b67cf9d8
veselov1209 <veselov1209@gmail.com>
2022-05-08 10:49:57 +0300

    простые операции

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,14 @@ console.log("Hello", "Java Script", "!!!!!!")
 */
 
 //переменные
+
+// Объявление переменных
 let x;
 let y;
 let userName;
 let userColorAvatarBG;
 let a, b, c;
-
+//  инициализация переменных
 x = 5;
 y = -100;
 a = 3.14;
@@ -23,3 +25,51 @@ console.log(x);
 console.log(y, a, b);
 console.log("User name is", userName);
 console.log("a =", a);
+
+//объявление с инициализацией
+
+let d = 200;
+let name1 = "Tom Fox";
+console.log(name1);
+
+// что можно задать в переменную
+
+let x2;
+
+x2 = 100;
+console.log(x2);
+x2=-3;
+console.log(x2);
+x2="hello";
+console.log(x2);
+x2=true;
+console.log(x2);
+
+//операции
+x = 10;
+x = x + 5;
+console.log('x=', x)
+x = x - 5;
+console.log('x=', x)
+x = x * 5;
+console.log('x=', x)
+x = x / 5;
+console.log('x=', x)
+
+let r = 10;
+let f = 0;
+let str = 'Sasha';
+
+let q = false, h = -2;
+
+let result;
+
+result = r + h;
+console.log(result)
+
+let result2;
+result2 = result * 2;
+console.log(result2)
+
+result2 = (500 - result2);
+console.log(result2)
